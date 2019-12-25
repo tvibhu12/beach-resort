c9d2a877c15b4ea1a223b4f2a2a13dbb79af6422
tvibhu12 <tvibhu12@gmail.com>
2019-12-25 15:31:50 +0530

    add react-router-dom in our project for navigation between pages

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import Home from './pages/Home';
 import Rooms from './pages/Rooms'
 import SingleRoom from './pages/SingleRoom';
 import Error from './pages/Error';
+import {Route,Switch} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
+
     <>
+    <Switch>
     hello from app
-    <Home/>
-    <Rooms/>
-    <SingleRoom/>
-    <Error/>
+<Route exact path='/' component={Home}/>
+   
+    <Route exact path='/rooms/' component={Rooms}/>
+    <Route exact path='/rooms/:single' component={SingleRoom}/>
+    <Route component={Error}/>
+    
+    </Switch>
     </>
   );
 }
